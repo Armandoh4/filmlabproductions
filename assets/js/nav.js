3c6ab4747a631228f901e9d34fc70c954eb9992c
Armando Halilaj <armandohalilj4@hotmail.com>
2023-12-10 14:13:23 +0000

    Javascript cleanup

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -36,26 +36,24 @@ setInterval(function() {
 },
 4000);
 
+
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
     const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
-
     const activeSlide = slides.querySelector("[data-active]");
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
     if (newIndex < 0) newIndex = slides.children.length - 1;
     if (newIndex >= slides.children.length) newIndex = 0;
-
     slides.children[newIndex].dataset.active = true;
     delete activeSlide.dataset.active;
   });
 });
-
-// Auto scroll function -> Temporarily out of use
-// const autoScroll = () => {
-//   const nextButton = document.querySelector("[data-carousel-button='next']");
-//   nextButton.click();
-// }
+// Auto scroll function
+const autoScroll = () => {
+  const nextButton = document.querySelector("[data-carousel-button='next']");
+  nextButton.click();
+}
 
 // Homepage background image alternations
 let backgroundImage1 = document.getElementById('backgroundImage1');
