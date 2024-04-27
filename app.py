2de1263a0e07b83106413aa5fe035a3dd08fe127
Armando Halilaj <107197449+Armandoh4@users.noreply.github.com>
2024-04-27 22:18:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-
+import os
 
 app = Flask(__name__)
 
@@ -125,4 +125,4 @@ def edit_film(film_id):
         return 'Film not found', 404
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=os.environ['PORT'])
