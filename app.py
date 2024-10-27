05cfa6c460d401bc86d9dfe8a56adeab2686d8fa
Armando Halilaj <armandohalilaj4@hotmail.com>
2024-10-28 00:43:37 +0100

    Port changed for hosting

--- app.py
+++ app.py
@@ -192,6 +192,6 @@ def edit_film(film_id):
 
 if __name__ == "__main__":
     app.config['DEBUG'] = False
-    host = os.environ.get("IP", "127.0.0.1")
+    host = os.environ.get("IP", "0.0.0.0") #Default port is 127.0.0.1
     port = os.environ.get("PORT") if os.environ.get("PORT") else 5000
-    app.run(host="127.0.0.1", port=port)  # View site on this link http://127.0.0.1:5000
+    app.run(host="0.0.0.0", port=port)  # View site on this link http://127.0.0.1:5000
