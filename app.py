1bfc95b4b026b78de7d23bed29c8f8e738872aa1
Armando Halilaj <armandohalilaj4@hotmail.com>
2024-11-07 19:33:02 +0800

    V2.1 STABLE: BUG FIXES - Large Push

---
+++ app.py
@@ -0,0 +1,197 @@
+import json
+import os
+from flask import Flask, render_template, request, redirect, url_for, session
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+
+
+load_dotenv()  # Here I set a secret key for session management
+app.secret_key = os.environ.get('APP_SECRET_KEY')
+admin_user = os.environ.get('USERNAMES')
+admin_pass = os.environ.get('PASSWORD')
+
+
+# Load films from JSON file
+def load_films():
+    with open('films.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+# Save films to JSON file
+def save_films(films):
+    with open('films.json', 'w', encoding='utf-8') as file:
+        json.dump(films, file, indent=4)
+        
+
+films = load_films()
+
+
+@app.route("/")
+@app.route("/home")
+@app.route("/index")
+def home():
+    films = load_films()  # Reload films
+    return render_template("index.html", latest_films=films)
+
+
+@app.route("/films")
+def showfilms():    
+    films = load_films()  # Reload films
+    return render_template("films.html", films=films)
+
+
+@app.route("/details/<int:film_id>")
+def film_details(film_id):
+    global films
+    for film in films:
+        if film['id'] == film_id:
+            film_detail = film
+    films = load_films()  # Reload films
+    return render_template("film_details.html", film=film_detail, films=films)
+
+
+@app.route("/bts")
+def bts():
+    films = load_films()  # Reload films
+    return render_template("bts.html")
+
+
+@app.route("/collaborations")
+def collaborations():
+    films = load_films()  # Reload films
+    return render_template("collaborations.html", films=films)
+
+
+@app.route("/about")
+def about():
+    films = load_films()  # Reload films
+    return render_template("about.html", films=films)
+
+
+@app.route("/login", methods=['GET'])
+def login():
+    films = load_films()  # Reload films
+    return render_template("login.html")
+
+
+# Route for handling the login form submission
+@app.route('/login', methods=['POST'])
+def login_route():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == admin_user and admin_pass == password:
+        # Authentication successful
+        session['username'] = username
+        # Redirect to upload page after successful login
+        return redirect(url_for('upload'))
+    else:
+        # Authentication failed
+        render_template('login.html', error='Invalid username or password')
+
+
+# Route for logging out
+@app.route('/logout')
+def logout():
+    session.pop('username', None)  # Remove username from session
+    return redirect(url_for('login'))  # Redirect to login page after logout
+
+
+next_id = max(film['id'] for film in films) + 1 if films else 1
+# ID for the next film entry
+
+
+# Route for rendering the upload page
+@app.route('/upload', methods=['GET'])
+def upload():
+    if 'username' in session:
+        # Render the upload page if user is logged in
+        films = load_films()  # Reload films
+        next_id = max(film['id'] for film in films) + 1 if films else 1
+        return render_template('upload.html', films=films)
+    else:
+        # Redirect to login page if user is not logged in
+        return redirect(url_for('login'))
+
+
+# Route for adding a new film
+@app.route('/add_film', methods=['POST'])
+def add_film():
+    films = load_films()  # Reload films
+    next_id = max(film['id'] for film in films) + 1 if films else 1
+
+    title = request.form.get('title')
+    description = request.form.get('description')
+    genre = request.form.get('genre')
+    duration = request.form.get('duration')
+    release_year = request.form.get('release_year')
+    director = request.form.get('director')
+    cast = request.form.get('cast').split(',')
+    link = request.form.get('link')
+    thumbnail = request.form.get('thumbnail')  # Get the thumbnail URL
+    
+    # Add the new film to the films list
+    films.append({
+        'id': next_id,
+        'title': title,
+        'description': description,
+        'genre': genre,
+        'duration': duration,
+        'release_year': int(release_year),
+        'director': director,
+        'cast': cast,
+        'link': link,
+        'thumbnail': thumbnail  # Add the thumbnail URL to the film entry
+    })
+    save_films(films)  # Save films to JSON file
+    
+    # Redirect to the upload page after adding the film
+    return redirect(url_for('upload'))
+
+
+
+# Route for deleting a film
+@app.route('/delete_film/<int:film_id>', methods=['GET'])
+def delete_film(film_id):
+    films = load_films()  # Reload films
+    # Remove the film with the given ID
+    films = [film for film in films if film['id'] != film_id]
+    save_films(films)  # Save films to JSON file
+    
+    # Redirect to the upload page after deleting the film
+    return redirect(url_for('upload'))
+
+
+
+# Route for editing a film
+@app.route('/edit_film/<int:film_id>', methods=['GET', 'POST'])
+def edit_film(film_id):
+    film = next((film for film in films if film['id'] == film_id), None)
+    if film:
+        if request.method == 'POST':
+            film['title'] = request.form.get('title')
+            film['description'] = request.form.get('description')
+            film['genre'] = request.form.get('genre')
+            film['duration'] = request.form.get('duration')
+            film['release_year'] = int(request.form.get('release_year'))
+            film['director'] = request.form.get('director')
+            film['cast'] = request.form.get('cast').split(',')
+            film['link'] = request.form.get('link')
+            # Update the thumbnail URL
+            film['thumbnail'] = request.form.get('thumbnail')
+            save_films(films)  # Save films to JSON file
+            # Redirect to the upload page after editing the film
+            return redirect(url_for('upload'))
+        else:
+            # Render the edit film page
+            return render_template('edit_film.html', film=film)
+    else:
+        return 'Film not found', 404
+
+
+if __name__ == "__main__":
+    app.config['DEBUG'] = False
+    host = os.environ.get("IP", "0.0.0.0") #Default port is 127.0.0.1
+    port = os.environ.get("PORT") if os.environ.get("PORT") else 5000
+    app.run(host="0.0.0.0", port=port)  # View site on this link http://127.0.0.1:5000
