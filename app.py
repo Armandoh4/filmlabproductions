bcb23d166bfd22c22cae3f052738c301f56bf21b
Armando Halilaj <armandohalilaj4@hotmail.com>
2024-11-29 18:04:48 +0000

    Simplification of code

--- app.py
+++ app.py
@@ -192,7 +192,6 @@ def edit_film(film_id):
 
 if __name__ == "__main__":
     app.config['DEBUG'] = False
-    host = os.environ.get("IP", "0.0.0.0") #Default port is 127.0.0.1
-    port = os.environ.get("PORT") if os.environ.get("PORT") else 5000
-    app.run(host="0.0.0.0", port=port)  # View site on this link http://127.0.0.1:5000
-    
+    host = os.environ.get("IP", "0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host=host, port=port)  # View site on this link http://0.0.0.0:5000
