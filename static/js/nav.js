79e8a2bdb0abc75921fc660de6d52b094b6cc53c
Armando Halilaj <armandohalilj4@hotmail.com>
2026-02-25 22:09:22 +0000

    Navigation bar Animation changed so it is no longer hidden - now appears from offscreen in a fixed position

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,5 +1,5 @@
- /* sidebar show */
- // Start of my own unique code
+/* sidebar show */
+// Start of my own unique code
 
 let burgerIcon = document.querySelector(".burgerIcon");
 let navbar = document.querySelector(".navbar");
@@ -11,37 +11,37 @@ let burgerIconPopup = document.querySelector(".burgerIconPopup");
 
 // sidebar show
 if (burgerIcon) {
-    burgerIcon.addEventListener("click", function() {
-        burgerIcon.style.display = "none";
-        if (navbar) navbar.style.display = "flex";
-        if (navbarMobile) navbarMobile.style.display = "none";
-    });
+  burgerIcon.addEventListener("click", function () {
+    if (navbar) {
+      navbar.classList.add("show-menu");
+    }
+  });
 }
 
 // sidebar hide
 if (xBurger) {
-    xBurger.addEventListener("click", function() {
-        if (navbar) navbar.style.display = "none";
-        if (navbarMobile) navbarMobile.style.display = "flex";
-        burgerIcon.style.display = "block";
-    });
+  xBurger.addEventListener("click", function () {
+    if (navbar) {
+      navbar.classList.remove("show-menu");
+    }
+  });
 }
 
 
 // Homepage background image alternations
-if (document.getElementById('backgroundImage1')){
+if (document.getElementById('backgroundImage1')) {
   let backgroundImage1 = document.getElementById('backgroundImage1');
   let originalSrc = backgroundImage1.src;
-  setInterval(function() {
+  setInterval(function () {
     if (backgroundImage1.src == originalSrc) {
       backgroundImage1.style.animation = 'fadeOut 1s';
-      setTimeout(function() {
+      setTimeout(function () {
         backgroundImage1.src = "/static/images/posterlastplaceonearth.png";
         backgroundImage1.style.animation = 'fadeIn 3s';
       }, 1000);
     } else {
       backgroundImage1.style.animation = 'fadeOut 1s';
-      setTimeout(function() {
+      setTimeout(function () {
         backgroundImage1.src = originalSrc;
         backgroundImage1.style.animation = 'fadeIn 3s';
       }, 1000);
@@ -52,36 +52,36 @@ if (document.getElementById('backgroundImage1')){
 
 // Banner under home image
 if (banner) {
-    banner.addEventListener('click', function() {
-        if (popup) popup.style.display = 'block'; // Show the popup
-    });
+  banner.addEventListener('click', function () {
+    if (popup) popup.style.display = 'block'; // Show the popup
+  });
 }
 
 if (burgerIconPopup) {
-    burgerIconPopup.addEventListener('click', function() {
-        if (popup) popup.style.display = 'none'; // Hide the popup
-    });
+  burgerIconPopup.addEventListener('click', function () {
+    if (popup) popup.style.display = 'none'; // Hide the popup
+  });
 }
 
 // BTS page
 // Carousel
 const buttons = document.querySelectorAll("[data-carousel-button]");
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        console.log("clicked");
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
-        if (slides) {
-            const activeSlide = slides.querySelector("[data-active]");
-            if (activeSlide) {
-                let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-                if (newIndex < 0) newIndex = slides.children.length - 1;
-                if (newIndex >= slides.children.length) newIndex = 0;
-                slides.children[newIndex].dataset.active = true;
-                delete activeSlide.dataset.active;
-            }
-        }
-    });
+  button.addEventListener("click", () => {
+    console.log("clicked");
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+    if (slides) {
+      const activeSlide = slides.querySelector("[data-active]");
+      if (activeSlide) {
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+      }
+    }
+  });
 });
 
 // carousel code taken from here https://www.youtube.com/watch?v=9HcxHDS2w1s):
