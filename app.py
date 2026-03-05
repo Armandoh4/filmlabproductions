86b117842bd7e0dec2df9f008da85be1b07c814e
Armando Halilaj <armandohalilaj4@hotmail.com>
2026-03-05 16:59:34 +0000

    poster change

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def edit_film(film_id):
 
 
 if __name__ == "__main__":
-    app.config['DEBUG'] = True
+    app.config['DEBUG'] = False
     host = os.environ.get("IP", "0.0.0.0")
     port = int(os.environ.get("PORT", 5000))
     app.run(host=host, port=port)  # View site on this link http://0.0.0.0:5000
